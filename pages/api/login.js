2ab469172094975c518b004a76d03e1599b9ed3c
Löffel Kimi <kimi.loeffel@bbcag.ch>
2022-04-20 15:40:47 +0200

    works now

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,7 +2,7 @@ import config from "@lib/sessionConfig";
 import { withIronSessionApiRoute } from "iron-session/next";
 const bcrypt = require("bcrypt");
 const models = require("@lib/server");
-const { User } = models.default;
+const { User, Role } = models.default;
 
 export default withIronSessionApiRoute(handler, config);
 
@@ -12,9 +12,9 @@ export async function handler(req, res) {
         method,
     } = req;
     switch (method) {
-    case "POST":
+    case "POST": {
         let user = await User.findOne( { where: { username }, include: [{ model: Role, as: "role_id_roles" }]});
-        if (user === null) user.findOne( { where: { email }, include: [{ model: Role, as: "role_id_roles" }]}); 
+        if (user === null) User.findOne( { where: { email }, include: [{ model: Role, as: "role_id_roles" }]}); 
         if (user === null) {
             res.status(404).json({ message: "User not found!"});
             return;
@@ -36,6 +36,8 @@ export async function handler(req, res) {
         };
         await req.session.save();
         res.send({ session: req.session });
+    }
+        break;
     default:
         res.setHeader("Allow", ["POST"]);
         res.status(405).end(`Method ${method} Not Allowed`);
