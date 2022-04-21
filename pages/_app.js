d1a4f4f7df53b242f5ce22360ff8a6826ffe3b56
Löffel Kimi <kimi.loeffel@bbcag.ch>
2022-04-21 15:32:13 +0200

    Removed some stuff I currently don't need

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,10 @@
-// import useSession from "@lib/session"
-import "./_app.css";
+import React from "react";
 
 export default function App({ Component, pageProps }) {
-    // const session = useSession()
-    // const newPageProps = {
-    //     ...pageProps,
-    //     session
-    // }
     return (
         <>
             <main className="page">
-                <Component {...newPageProps} />
+                <Component {...pageProps} />
             </main>
         </>
     );
