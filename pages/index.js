450219bd5a91ce2b4aa4f50264f5d7b99367d912
Löffel Kimi <kimi.loeffel@bbcag.ch>
2022-04-22 14:50:17 +0200

    Index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
 import React from "react";
+import Link from "next/link";
+import { Alert, Card, Container } from "react-bootstrap";
 
-export default function IndexPage() {
+export default function IndexPage({ data }) {
+    if (!data.tasks) {
+        return (
+            <Container>
+                <Alert variant="info">Loading...</Alert>
+            </Container>
+        );
+    }
     return (
-        <div>
-        </div>
+        <Container>
+            <h1 className="mb-5">Tasks:</h1>
+            {
+                data.tasks.map(task => {
+                    return (
+                        <Card key={task.id} className="mb-3">
+                            <Card.Header className="p-0">
+                                <h5 className="mb-0 p-2">
+                                    <Link 
+                                        href={`tasks/${
+                                            task.name.replace(/\s/g, "")
+                                        }`}
+                                    >
+                                        <a className="text-dark">
+                                            {task.name}
+                                        </a>
+                                    </Link>
+                                </h5>
+                            </Card.Header>
+                        </Card>
+                    );
+                })
+            }
+        </Container>
     );
 }
