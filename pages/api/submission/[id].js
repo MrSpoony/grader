57846c83f6189f2ae8d5f7602b0f93a820aedd32
Löffel Kimi <kimi.loeffel@bbcag.ch>
2022-04-28 11:02:33 +0200

    Submission getting is optimized

--- pages/api/submission/[id].js
+++ pages/api/submission/[id].js
@@ -19,7 +19,7 @@ export async function handler(req, res) {
     } = req;
     const submission = await Submission.findByPk(id);
     if (!submission) {
-        res.status(404).json({message: "Submission not found" });
+        res.status(404).json({ message: "Submission not found" });
         return;
     }
     if ((req.session?.user?.id !== submission.user_id &&
@@ -31,77 +31,81 @@ export async function handler(req, res) {
     }
     switch (method) {
     case "GET": {
+
+        let submission = await Submission.findByPk(id);
         let sampleCases = await Submission.findByPk(id, {
             include: [
-                { 
+                {
                     model: TestcaseStatus, as: "testcase_statuses",
-                    include: [
-                        { model: Testcase, as: "testcase",
-                            include: [
-                                {
-                                    model: Testgroup, as: "testgroup",
-                                    include: [
-                                        {
-                                            model: Testgrouptype,
-                                            as: "testgrouptype",
-                                            where: {
-                                                type: "sample"
-                                            },
-                                            required: false,
-                                            right: true
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
+                    required: false,
+                    include: [{
+                        model: Testcase, as: "testcase",
+                        required: false,
+                        include: [{
+                            model: Testgroup, as: "testgroup",
+                            required: false,
+                            include: [{
+                                model: Testgrouptype,
+                                as: "testgrouptype",
+                                where: {
+                                    type: "sample"
+                                },
+                                required: false,
+                                right: true
+                            }
+                            ]}
+                        ]}
                     ]
                 },
+                { model: Task, as: "task" },
             ]
         });
+
         let realCases = await Submission.findByPk(id, {
             include: [
-                { 
+                {
                     model: TestcaseStatus, as: "testcase_statuses",
+                    required: false,
                     attributes: [
                         "id",
                         "status_id",
                         "testcase_id",
                         "submission_id"
                     ],
-                    include: [
-                        { model: Testcase, as: "testcase",
-                            attributes: ["testgroup_id"],
-                            include: [
-                                {
-                                    model: Testgroup, as: "testgroup",
-                                    include: [
-                                        {
-                                            model: Testgrouptype,
-                                            as: "testgrouptype",
-                                            where: {
-                                                type: "real"
-                                            },
-                                            required: false,
-                                            right: true
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
+                    include: [{
+                        model: Testcase, as: "testcase",
+                        required: false,
+                        attributes: ["testgroup_id"],
+                        include: [{
+                            model: Testgroup, as: "testgroup",
+                            required: false,
+                            include: [{
+                                model: Testgrouptype,
+                                as: "testgrouptype",
+                                where: {
+                                    type: "real"
+                                },
+                                required: false,
+                                right: true
+                            }]
+                        }]
+                    }]
                 },
                 { model: Task, as: "task" },
             ]
         });
-        sampleCases = sampleCases.get({ plain: true });
-        realCases = realCases.get({ plain: true });
-        let everything = {
-            ...sampleCases,
-        };
-        everything.testcase_statuses = [
-            ...everything.testcase_statuses,
-            ...realCases.testcase_statuses
-        ];
+
+        sampleCases = await sampleCases?.get({ plain: true });
+        realCases = await realCases?.get({ plain: true });
+        submission = await submission.get({ plain: true });
+        let everything = {};
+        if (!sampleCases) everything = submission;
+        else everything = sampleCases;
+        if (realCases?.testcase_statuses)
+            everything.testcase_statuses = [
+                ...everything.testcase_statuses,
+                ...realCases.testcase_statuses
+            ];
         res.status(200).json(everything);
     }
         break;
@@ -112,7 +116,7 @@ export async function handler(req, res) {
             res.status(401).json({ message: "Unauthorized" });
             return;
         }
-        res.status(200).json(await Submission.destroy({ where: { id }}));
+        res.status(200).json(await Submission.destroy({ where: { id } }));
         break;
     case "PUT":
         res.status(200).json(await Submission.update(
